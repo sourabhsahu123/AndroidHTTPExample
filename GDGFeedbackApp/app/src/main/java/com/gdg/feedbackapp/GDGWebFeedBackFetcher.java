d36afccd795aa4df9bd542d15f18a54e8bb15a6c
sourabh sahu <sourabh_sahu@live.in>
2020-04-05 17:30:15 +0530

    four commit with volley

--- GDGFeedbackApp/app/src/main/java/com/gdg/feedbackapp/GDGWebFeedBackFetcher.java
+++ GDGFeedbackApp/app/src/main/java/com/gdg/feedbackapp/GDGWebFeedBackFetcher.java
@@ -6,6 +6,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -25,7 +26,7 @@ import java.util.ArrayList;
 
 public class GDGWebFeedBackFetcher extends AppCompatActivity {
 
-    String gdgfeedbackurl="https://raw.githack.com/sourabhsahu123/gdgcontent/master/gdg.json";
+    String gdgfeedbackurl="https://my-json-server.typicode.com/typicode/demo/posts";
     FeedbackAdapter fadapter;
     RecyclerView feedbackRCV;
     @Override
@@ -52,6 +53,11 @@ public class GDGWebFeedBackFetcher extends AppCompatActivity {
         });
 
         ArrayList<GDGFeedback>fList = fetchDataFromWeb(gdgfeedbackurl);
+        try {
+            ArrayList<GDGFeedback> fList2 = postdata(gdgfeedbackurl);
+        }catch (Exception ex){
+
+        }
         System.out.println(fList);
     }
 
@@ -101,4 +107,68 @@ public class GDGWebFeedBackFetcher extends AppCompatActivity {
 
     return  gfList;
     }
+
+
+    public ArrayList<GDGFeedback> postdata(String feedbackUrl) throws Exception{
+
+
+        final ArrayList<GDGFeedback> gfList= new ArrayList<GDGFeedback>();
+    //
+
+        JSONObject jpost = new JSONObject("{\n" +
+                "\"feedback\":[\n" +
+                "{\"name\":\"Sourabh\",\"occupation\":\"ITProfessional\",\"rating\":5.0,\"qualification\":\"MTech\",\"suggestion\":\"na\",\"age\":30,\"isAgree\":\"False\"},\n" +
+                "{\"name\":\"Ankit\",\"occupation\":\"IT Professional\",\"rating\":5.0,\"qualification\":\"BE\",\"suggestion\":\"na\",\"age\":30,\"isAgree\":\"False\"},\n" +
+                "{\"name\":\"Ram\",\"occupation\":\"IT Professional\",\"rating\":2.0,\"qualification\":\"BE\",\"suggestion\":\"na\",\"age\":30,\"isAgree\":\"False\"}\n" +
+                "\n" +
+                "]\n" +
+                "}");
+        try {
+            JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, feedbackUrl,jpost, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject jso = response;
+                        JSONArray jr = jso.getJSONArray("feedback");
+                        for (int i = 0; i < jr.length(); i++) {
+
+                            JSONObject jso1 = jr.getJSONObject(i);
+                            GDGFeedback gf = new GDGFeedback(jso1.getString("name"), jso1.getString("occupation"), jso1.getInt("rating"),
+                                    jso1.getString("qualification"), jso1.getString("suggestion"), jso1.getInt("age"), false);
+                            gfList.add(gf);
+                            fadapter = new FeedbackAdapter(gfList);
+                            feedbackRCV.setAdapter(fadapter);
+                            fadapter.notifyDataSetChanged();
+                        }
+
+
+                        System.out.print(jso.toString());
+                    } catch (Exception ex) {
+                        System.out.println(ex.toString());
+                    }
+                }
+
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println(error.toString());
+                        }
+                    }
+
+            );
+            //Create a Queue
+            RequestQueue reqQueue= Volley.newRequestQueue(this);
+            //add a request to the queue
+            reqQueue.add(stringRequest);
+            // StringRequest stringRequest = new StringRequest()
+        }
+        catch (Exception ex) {
+        }
+
+
+
+    return  gfList;
+}
+
 }
